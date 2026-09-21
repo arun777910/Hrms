85bf30762d08f3447c7504d8cad11d078c4c843f
arun777910 <arunsakkaravarthy777@gmail.com>
2026-09-21 11:16:35 +0530

    Update and rename Login_test.java to LoginTest.java

--- arun.first.maven/src/test/java/hrms_tests/LoginTest.java
+++ arun.first.maven/src/test/java/hrms_tests/LoginTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 import hrms_base.Base;
 import hrms_pages.Login_page;
 
-public class Login_test extends Base 
+public class LoginTest extends Base 
 {
 	Login_page loginPage;
 	
