d9fd0a61b75494a71ecc36c989ad6f6dec0e9138
arun777910 <arunsakkaravarthy777@gmail.com>
2026-09-23 10:17:22 +0530

    Create BaseCls.java

---
+++ arun.first.maven/src/test/java/hrms_base/BaseCls.java
@@ -0,0 +1,46 @@
+ackage hrms_base;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class BaseCls {
+	
+	
+	protected Playwright playwright;
+	protected Browser browser;
+	protected BrowserContext context;
+	protected Page page;
+	
+	@BeforeClass	
+	public void setUp() 
+	{
+	playwright = Playwright.create();
+
+	browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+	
+	context = browser.newContext();
+	
+	page = context.newPage();
+	
+	}
+
+	@AfterClass	
+	public void tearDown() {
+
+	context.close();
+	
+	browser.close();
+
+	playwright.close();
+	
+	}
+
+}
